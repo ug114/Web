0c04264a154ac61bc22304740d0c326c1f95a47c
user <user@WIN-2KR6UHLB0NV>
2018-08-20 14:04:42 +0400

    Small changes in array.

--- Web/Web/Gabriel/script.js
+++ Web/Web/Gabriel/script.js
@@ -1,28 +1,24 @@
 document.addEventListener("DOMContentLoaded", function () {
-    $(function () {
-        var sliders = $(".slider");
+    var sliders = $(".slider");
 
-        sliders.each(function () {
-            value = Number($(this).text(), 10);
-
-            $(this).empty().slider({
-                value: 0,
-                min: 0,
-                max: 1000,
-                step: 10,
-                animate: 100,
-                slide: function (event, ui) {
-                    $(this).siblings(".slider-value").attr("value", ui.value);
-                    $(this).siblings(".slider-value").trigger("change");
-                }
-            });
+    sliders.each(function () {
+        $(this).empty().slider({
+            value: 0,
+            min: 0,
+            max: 1000,
+            step: 10,
+            animate: 100,
+            slide: function (event, ui) {
+                $(this).siblings(".slider-value").attr("value", ui.value);
+                $(this).siblings(".slider-value").trigger("change");
+            }
         });
+    });
 
-        $(".slider-value").change(function () {
-            $(".slider-value").each(function () {
-                var sliderVal = Number($(this).prop("value"));
-                $(this).siblings(".slider").slider("option", "value", sliderVal);
-            });
+    $(".slider-value").change(function () {
+        $(".slider-value").each(function () {
+            var sliderVal = Number($(this).prop("value"));
+            $(this).siblings(".slider").slider("option", "value", sliderVal);
         });
     });
 });
